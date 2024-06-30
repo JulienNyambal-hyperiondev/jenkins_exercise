458f2e42913366f9dcd3e17661ef4bd8a5adc2fd
Julien Nyambal <julienn@hyperiondev.com>
2024-06-30 16:18:45 +0200

    update code with more tests, and more functions

--- my_calculator/calc_app.py
+++ my_calculator/calc_app.py
@@ -10,6 +10,43 @@ def addition(x: int, y: int) -> int:
     z = x + y
     return z
 
+def multiplication(x: int, y: int) -> int:
+    """multiplication
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        int: int
+    """
+    return x * y
+
+def division(x, y):
+    """division
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        int: int
+    """
+    return x / y
+
+def subtraction(x, y):
+    """subtraction
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        int: int
+    """
+    return x - y
+
+
 if __name__ == "__main__":
     # result should be 2005
     print(addition(2000, 5))
